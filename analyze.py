9c452caea7a6d79ac5867e09e475cc8e30289345
Bernd Prager <bernd@prager.ws>
2023-05-19 10:03:59 -0700

    different aproach

---
+++ analyze.py
@@ -0,0 +1,22 @@
+import json
+import sys
+
+
+def analyze_file(file):
+    print("Analyzing file:", file["file"])
+
+    if "content" in file:
+        print("Content:")
+        print(file["content"])
+    if "diff" in file:
+        print("Diff:")
+        print(file["diff"])
+
+    # Perform your analysis on the file here
+
+
+if __name__ == "__main__":
+    files = json.loads(sys.argv[1])
+
+    for file in files:
+        analyze_file(file)
