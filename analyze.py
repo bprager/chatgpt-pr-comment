e3872777632e79ca00bbc4476a5a9cca0fa0d9a4
Bernd Prager <bernd@prager.ws>
2023-05-20 12:17:28 -0700

    "check file exist and readable"

--- analyze.py
+++ analyze.py
@@ -15,27 +15,25 @@ def analyze_files(temp_dir, added_files, modified_files=[], diff_files=[]):
     # Process added files
     print("Added files:", added_files)
     for file in added_files:
-        if not file:
-            continue
         file_path = os.path.join(temp_dir, file)
-        analyze_file(file_path)
+        if os.isfile(file_path) and os.access(file_path, os.R_OK):
+            analyze_file(file_path)
 
     # Process modified files
     print("Modified files:", modified_files)
     for file in modified_files:
-        if not file:
-            continue
         file_path = os.path.join(temp_dir, file)
-        analyze_file(file_path)
+        if os.isfile(file_path) and os.access(file_path, os.R_OK):
+            analyze_file(file_path)
 
     # Process diff files
     print("Diff files:", diff_files)
     for diff_file in diff_files:
-        if not diff_file:
-            continue
-        with open(os.path.join(temp_dir, diff_file), "r") as file:
-            diff_content = file.read()
-            analyze_diff(diff_content)
+        diff_file_path = os.path.join(temp_dir, diff_file)
+        if os.isfile(diff_file_path) and os.access(diff_file_path, os.R_OK):
+            with open(diff_file_path, "r") as file:
+                diff_content = file.read()
+                analyze_diff(diff_content)
 
 
 if __name__ == "__main__":
