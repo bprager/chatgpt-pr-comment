2c687e90a778d203a35018a188612dd68b55a80a
Bernd Prager <bernd@prager.ws>
2023-05-20 12:06:22 -0700

    "check for empty filenames"

--- analyze.py
+++ analyze.py
@@ -15,18 +15,24 @@ def analyze_files(temp_dir, added_files, modified_files=[], diff_files=[]):
     # Process added files
     print("Added files:", added_files)
     for file in added_files:
+        if not file:
+            continue
         file_path = os.path.join(temp_dir, file)
         analyze_file(file_path)
 
     # Process modified files
     print("Modified files:", modified_files)
     for file in modified_files:
+        if not file:
+            continue
         file_path = os.path.join(temp_dir, file)
         analyze_file(file_path)
 
     # Process diff files
     print("Diff files:", diff_files)
     for diff_file in diff_files:
+        if not file:
+            continue
         with open(os.path.join(temp_dir, diff_file), "r") as file:
             diff_content = file.read()
             analyze_diff(diff_content)
